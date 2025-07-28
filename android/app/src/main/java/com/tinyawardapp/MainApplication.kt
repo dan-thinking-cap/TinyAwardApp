e09fbcf7795c3d6738c6ab0a00081e9fc08a0823
Danish Fahim <dan@thinkingcap.com>
2025-07-28 12:28:15 -0400

    Added patch file for react-unity-view library to work with Unity-2023+ and finished configuring files

--- android/app/src/main/java/com/tinyawardapp/MainApplication.kt
+++ android/app/src/main/java/com/tinyawardapp/MainApplication.kt
@@ -3,25 +3,36 @@ package com.tinyaward.app
 import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
+import com.facebook.react.ReactHost
+import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.soloader.SoLoader
+import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import com.facebook.react.defaults.DefaultReactNativeHost
 
 class MainApplication : Application(), ReactApplication {
 
-  override val reactNativeHost: ReactNativeHost = object : ReactNativeHost(this) {
-    override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+  override val reactNativeHost: ReactNativeHost =
+      object : DefaultReactNativeHost(this) {
+        override fun getPackages(): List<ReactPackage> =
+            PackageList(this).packages.apply {
+              // Packages that cannot be autolinked yet can be added manually here, for example:
+              // add(MyReactNativePackage())
+            }
 
-    override fun getPackages(): List<ReactPackage> =
-      PackageList(this).packages.apply {
-        // Add manually linked packages here, if any
+        override fun getJSMainModuleName(): String = "index"
+
+        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+
+        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
       }
 
-    override fun getJSMainModuleName(): String = "index"
-  }
+  override val reactHost: ReactHost
+    get() = getDefaultReactHost(applicationContext, reactNativeHost)
 
   override fun onCreate() {
     super.onCreate()
-    SoLoader.init(this, /* native exopackage */ false)
+    loadReactNative(this)
   }
 }
